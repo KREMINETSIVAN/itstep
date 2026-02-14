4127eeacd6038edfd3b696795b8aa8cba59d073b
Ivan <kreminecivan@gmail.com>
2026-02-14 10:06:14 +0200

    add to hello world

---
+++ homework3.py
@@ -0,0 +1,13 @@
+import colorama
+import inspect
+print(inspect.ismodule(colorama))
+print(inspect.isclass(colorama))
+print(inspect.getmodule(colorama))
+print(inspect.isfunction(colorama))
+print(inspect.getmodule(colorama))
+print(inspect.getmodule(list))
+import sys
+print(sys.executable)
+print(sys.version)
+print(sys.platform)
+print(sys.argv)
